562ed15bb4f473cbe18b1ea590ea2ca5eee02dfe
alessandro-pianetta <alessandro.pianetta@gmail.com>
2018-08-09 10:03:20 -0700

    Removes android slider

--- components/Scrubber.js
+++ components/Scrubber.js
@@ -22,32 +22,20 @@ const styles = StyleSheet.create({
 
 const Scrubber = props => {
   const trackColor = "rgba(255,255,255,0.5)";
-  const { progress, theme, onSeek, onSeekRelease, sliderStyle } = props;
+  const { progress, theme, onSeek, onSeekRelease } = props;
   return (
     <View style={styles.container}>
-      {Platform.OS === "ios" ? (
-        <Slider
-          onValueChange={val => onSeek(val)}
-          onSlidingComplete={val => onSeekRelease(val)}
-          value={progress === Number.POSITIVE_INFINITY ? 0 : progress}
-          thumbTintColor={theme.scrubberThumb}
-          thumbStyle={styles.thumbStyle}
-          trackStyle={styles.trackStyle}
-          minimumTrackTintColor={theme.scrubberBar}
-          maximumTrackTintColor={trackColor}
-          trackClickable
-        />
-      ) : (
-        <RNSlider
-          style={[styles.slider, sliderStyle]}
-          onValueChange={val => onSeek(val)}
-          onSlidingComplete={val => onSeekRelease(val)}
-          value={progress}
-          thumbTintColor={theme.scrubberThumb}
-          minimumTrackTintColor={theme.scrubberBar}
-          maximumTrackTintColor={trackColor}
-        />
-      )}
+      <Slider
+        onValueChange={val => onSeek(val)}
+        onSlidingComplete={val => onSeekRelease(val)}
+        value={progress === Number.POSITIVE_INFINITY ? 0 : progress}
+        thumbTintColor={theme.scrubberThumb}
+        thumbStyle={styles.thumbStyle}
+        trackStyle={styles.trackStyle}
+        minimumTrackTintColor={theme.scrubberBar}
+        maximumTrackTintColor={trackColor}
+        trackClickable
+      />
     </View>
   );
 };
@@ -56,8 +44,7 @@ Scrubber.propTypes = {
   onSeek: PropTypes.func.isRequired,
   onSeekRelease: PropTypes.func.isRequired,
   progress: PropTypes.number.isRequired,
-  theme: PropTypes.object.isRequired,
-  sliderStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.number])
+  theme: PropTypes.object.isRequired
 };
 
 export { Scrubber };
